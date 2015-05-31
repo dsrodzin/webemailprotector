4b027129082c09d2d0b36485d5364598e125ef6a
dsrodzin <david@webemailprotector.com>
2015-05-31 23:45:57 +0100

    Signed-off-by: dsrodzin <david@webemailprotector.com>

--- admin/webemailprotector_email_change.php
+++ admin/webemailprotector_email_change.php
@@ -1,10 +1,9 @@
 <?php
-//change email address in db
-
-function webemailprotector_email_change($i,$email){
-   variable_set('wepdb_wep_email_'.$i,$email);
-   variable_set('wepdb_wep_validated_'.$i,'false');
-   echo $email;
+/**
+ * Change the email address in the db.
+ */
+function webemailprotector_email_change($i, $email) {
+  variable_set('wepdb_wep_email_' . $i, $email);
+  variable_set('wepdb_wep_validated_' . $i, 'false');
+  echo $email;
 }
-
-?>
--- admin/webemailprotector_email_get.php
+++ admin/webemailprotector_email_get.php
@@ -1,9 +1,8 @@
 <?php
-//change email address in db
-
-function webemailprotector_email_get($i){
-   $email=variable_get('wepdb_wep_email_'.$i);
-   echo $email;
- }
-
-?>
+/** 
+ * Change the email address in the db.
+ */
+function webemailprotector_email_get($i) {
+  $email=variable_get('wepdb_wep_email_' . $i);
+  echo $email;
+}
